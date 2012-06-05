6d4eea93826bcfe53b8af3182aaff3e70425e63e
Sergey Zwezdin <sergey@zwezdin.com>
2012-06-05 10:36:57 +0600

    Initial dialog binding

---
+++ js/ui-dialog.js
@@ -0,0 +1,21 @@
+﻿/*global document, window, $, ko, debug, setTimeout, alert */
+ko.bindingHandlers.dialog = {
+    init: function (element, valueAccessor, allBindings, viewModel) {
+        $(element).dialog();
+
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        if (value == true)
+            $(element).dialog("open");
+        else
+            $(element).dialog("close");
+    },
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        var isDialogOpen = $(element).dialog("isOpen");
+
+        if ((value == true) && (isDialogOpen == false))
+            $(element).dialog("open");
+        else if ((value == false) && (isDialogOpen == true))
+            $(element).dialog("close");
+    }
+};
