d11c58b0ffbb6f4ffcac83942439cbbe186f037e
Ian Mckay <ian@avastmehearties.com>
2013-04-07 21:15:46 -0700

    Cleaning up dependency files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,31 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> - <%= pkg.description %> Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n'
+      },
+      min: {
+        files: {
+          'build/js/ui-tree.min.js': ['src/js/ui-tree.js'],
+          'build/js/ui-tabs.min.js': ['src/js/ui-tabs.js'],
+          'build/js/ui-util.min.js': ['src/js/ui-util.js'],
+          //'build/js/ui-dialog.min.js': ['src/js/ui-dialog.js'],
+          'build/js/ui-dropdown.min.js': ['src/js/ui-dropdown.js'],
+          'build/js/ui-autocomplete.min.js': ['src/js/ui-autocomplete.js']
+        }
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        options: {
+          mangle: false,
+          compress: false
+        },
+        files: {
+          'build/js/ui-tree.js': ['src/js/ui-tree.js'],
+          'build/js/ui-tabs.js': ['src/js/ui-tabs.js'],
+          'build/js/ui-util.js': ['src/js/ui-util.js'],
+          //'build/js/ui-dialog.js': ['src/js/ui-dialog.js'],
+          'build/js/ui-dropdown.js': ['src/js/ui-dropdown.js'],
+          'build/js/ui-autocomplete.js': ['src/js/ui-autocomplete.js']
+        }
       }
     }
   });
@@ -18,6 +38,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify:min', 'uglify:build']);
 
 };
